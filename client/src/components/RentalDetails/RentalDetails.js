94ac732c065119e9d8db6620f365c5bee6a0948e
nmemarcoding <nmemarcoding@gmail.com>
2023-07-27 09:44:47 -0700

    Add functionality to retrieve data from backend and display it on review page

--- client/src/components/RentalDetails/RentalDetails.js
+++ client/src/components/RentalDetails/RentalDetails.js
@@ -1,32 +1,36 @@
 import React from 'react'
 
-export default function RentalDetails() {
+export default function RentalDetails(props) {
+  const { make, model, color, seat, fuelType, pickUpDue, price } = props.car;
+
+
   return (
     <>
-      <div className="w-full md:w-1/2  p-4 md:p-10 rounded-lg shadow-lg">
-        <img className="w-full h-64 object-cover mb-4 rounded" src="https://assets.gcs.ehi.com/content/enterprise_cros/data/vehicle/bookingCountries/US/CARS/ECAR.doi.768.high.imageSmallThreeQuarterNodePath.png/1492780366644.png" alt="BMW X5" />
-        <h1 className="text-2xl font-bold text-gray-800 tracking-wide mb-4">BMW X5</h1>
-        <h2 className="text-lg font-bold text-gray-600 tracking-wide mb-8">WHITE</h2>
-        
+      <div className="w-full md:w-1/2 p-4 md:p-10 rounded-lg shadow-lg bg-white">
+        <img className="w-full h-64 object-cover mb-4 rounded" src="https://assets.gcs.ehi.com/content/enterprise_cros/data/vehicle/bookingCountries/US/CARS/ECAR.doi.768.high.imageSmallThreeQuarterNodePath.png/1492780366644.png" alt={`${make} ${model}`} />
+        <h1 className="text-3xl font-bold text-gray-800 tracking-wide mb-4">{`${make} ${model}`}</h1>
+        <h2 className="text-lg font-bold text-gray-600 tracking-wide mb-2">COLOR</h2>
+        <h3 className=" text-gray-600 tracking-wide mb-2">{color}</h3>
+        <div className="border-b-2 border-gray-300 mb-4\2"></div>
         <div className="border-b-2 border-gray-300 py-4">
           <h3 className="text-lg font-bold text-gray-600 tracking-wide">SEATING</h3>
-          <p className="text-gray-800">5</p>
+          <p className="text-gray-800">{seat}</p>
         </div>
         <div className="border-b-2 border-gray-300 py-4">
           <h3 className="text-lg font-bold text-gray-600 tracking-wide">FUEL TYPE</h3>
-          <p className="text-gray-800">GASOLINE</p>
+          <p className="text-gray-800">{fuelType}</p>
         </div>
         <div className="border-b-2 border-gray-300 py-4">
           <h3 className="text-lg font-bold text-gray-600 tracking-wide">PICK-UP DUE</h3>
-          <p className="text-gray-800">6 PM</p>
+         <p className="text-gray-800">{new Date().toLocaleDateString()} until 6pm</p>
         </div>
         <div className="border-b-2 border-gray-300 py-4">
           <h3 className="text-lg font-bold text-gray-600 tracking-wide">PRICE</h3>
-          <p className="text-gray-800">$85 <span className="text-sm">Per Day</span></p>
+          <p className="text-gray-800">{`$${price} Per Day`}</p>
         </div>
 
         <div className="flex justify-center py-4">
-          <button className="bg-yellow-500 text-white font-bold py-2 px-4 rounded mt-4 sm:mt-0 shadow-md">Reserve Now</button>
+          <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded mt-4 sm:mt-0 shadow-md">Reserve Now</button>
         </div>
       </div>
     </>
--- client/src/pages/ReviewPage/ReviewPage.js
+++ client/src/pages/ReviewPage/ReviewPage.js
@@ -6,14 +6,14 @@ import RentalDetails from '../../components/RentalDetails/RentalDetails'
 export default function ReviewPage() {
   // get car infor from rout that send by navigate
   const car = useLocation().state.car
-  console.log(car)
+  
 
   return (
     <div className="h-screen w-screen ">
       <Navbar/>
       <h1 class="text-4xl font-bold text-gray-800 tracking-wide py-10 px-7 border-b-2 border-gray-300">Rental Details</h1>
      <div className="flex w-full justify-center">
-        <RentalDetails/>
+        <RentalDetails car={car} />
       </div> 
     </div>
   )
