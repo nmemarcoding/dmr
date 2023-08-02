ae7b095253d4f9a87fd8990c25d0214aaa57654c
nmemarcoding <nmemarcoding@gmail.com>
2023-08-02 11:21:22 -0700

    desgin change

--- client/src/components/AdminReservedCarTable/AdminReservedCarTable.js
+++ client/src/components/AdminReservedCarTable/AdminReservedCarTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, handelReturn }) {
-
   const pickUpOnClick = () => {
     handelPickUp(order._id);
   };
@@ -10,9 +9,16 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
     handelReturn(order._id)
   };
   
+  // Function to check if orderTime is more than one day ago
+  const isOrderCancelled = () => {
+    const oneDayInMillis = 24 * 60 * 60 * 1000; // Number of milliseconds in one day
+    const orderTimeInMillis = new Date(order.orderTime).getTime();
+    const currentTimeInMillis = new Date().getTime();
+    return currentTimeInMillis - orderTimeInMillis > oneDayInMillis;
+  };
+  
   return (
     <div className='flex flex-col items-center sm:items-start w-full px-4 sm:px-0'>
-
       <div className='w-full overflow-auto sm:hidden'>
         <div className='border-b border-gray-300 p-4'>
           <div className='mb-2'>
@@ -32,9 +38,9 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
           </div>
           <div className='mb-2'>
             <strong className='font-bold'>Status: </strong>
-            {order.carId.available ? 'Available' : 'Not Available'}
+            {isOrderCancelled() ? 'Cancelled' : (order.carId.available ? 'Available' : 'Not Available')}
           </div>
-          {order.carId.available && !order.returnTime && !order.pickUpTime &&(
+          {order.carId.available && !order.returnTime && !order.pickUpTime && !isOrderCancelled() && (
             <button
               className='px-2 py-1 rounded bg-yellow-500 text-white hover:bg-yellow-700 focus:outline-none focus:shadow-outline-yellow'
               onClick={pickUpOnClick}
@@ -42,12 +48,12 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
               Pick Up
             </button>
           )}
-          {order.returnTime && order.pickUpTime &&(
-                  <div className=''>
-                    Returned
-                  </div>
-                )}
-          {order.pickUpTime && !order.returnTime && !order.carId.available &&(
+          {order.returnTime && order.pickUpTime && (
+            <div className=''>
+              Returned
+            </div>
+          )}
+          {order.pickUpTime && !order.returnTime && !order.carId.available && !isOrderCancelled() && (
             <button
               className='px-2 py-1 rounded bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:shadow-outline-red'
               onClick={returnOnClick}
@@ -75,29 +81,33 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
               <td className='px-2 sm:px-4 py-2'>{`${order.userId?.firstName} ${order.userId?.lastName}`}</td>
               <td className='px-2 sm:px-4 py-2'>{`${order.carId.make} ${order.carId.model}`}</td>
               <td className='px-2 sm:px-4 py-2'>{new Date(order.orderTime).toLocaleDateString('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'})} Until 6 PM</td>
-              <td className='px-2 sm:px-4 py-2'>{order.carId.available ? 'Available' : 'Not Available'}</td>
+              <td className='px-2 sm:px-4 py-2'>{isOrderCancelled() ? 'Cancelled' : (order.carId.available ? 'Available' : 'Not Available')}</td>
               <td className='px-2 sm:px-4 py-2'>
-                {order.carId.available && !order.returnTime && !order.pickUpTime &&(
+                {order.carId.available && !order.returnTime && !order.pickUpTime && !isOrderCancelled() && (
                   <button
                     className='px-2 py-1 rounded bg-yellow-500 text-white hover:bg-yellow-700 focus:outline-none focus:shadow-outline-yellow'
                     onClick={pickUpOnClick}
                   >
-                  Pick Up
-                    
+                    Pick Up
                   </button>
                 )}
-                {order.returnTime && order.pickUpTime &&(
+                {order.returnTime && order.pickUpTime && (
                   <div className=''>
                     Returned
                   </div>
                 )}
-                {order.pickUpTime && !order.returnTime && !order.carId.available &&(
+                {order.pickUpTime && !order.returnTime && !order.carId.available && !isOrderCancelled() && (
                   <button
                     className='px-2 py-1 rounded bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:shadow-outline-red'
                     onClick={returnOnClick}
                   >
-                     Return
+                    Return
                   </button>
+                )}
+                {isOrderCancelled() && (
+                  <div className='text-red-600'>
+                    Cancelled
+                  </div>
                 )}
               </td>
             </tr>
--- client/src/pages/OrderHistory/OrderHistory.js
+++ client/src/pages/OrderHistory/OrderHistory.js
@@ -4,100 +4,184 @@ import useStore from '../../store';
 import { publicRequest } from '../../hooks/requestMethods';
 
 export default function OrderHistory() {
-    const [userId, setUserId] = useState(useStore((state) => state?.userInfo?._id));
-    const [orders, setOrders] = useState([]);
+  const [userId, setUserId] = useState(useStore((state) => state?.userInfo?._id));
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => { 
-        publicRequest().get(`/order/getallorders?userid=${userId}`)
-        .then((res) => {
-            // Assuming the API response is an array of orders sorted by orderTime (newest first)
-            const sortedOrders = res.data.sort((a, b) => new Date(b.orderTime) - new Date(a.orderTime));
-            setOrders(sortedOrders);
-        })
-        .catch((err) => console.log(err));
-    }, [userId]); // Add the userId dependency to fetch orders when the userId changes
+  useEffect(() => {
+    publicRequest()
+      .get(`/order/getallorders?userid=${userId}`)
+      .then((res) => {
+        // Assuming the API response is an array of orders sorted by orderTime (newest first)
+        const sortedOrders = res.data.sort(
+          (a, b) => new Date(b.orderTime) - new Date(a.orderTime)
+        );
+        setOrders(sortedOrders);
+      })
+      .catch((err) => console.log(err));
+  }, [userId]); // Add the userId dependency to fetch orders when the userId changes
 
-    // Function to determine the status based on pickUpTime and returnTime
-    const getStatus = (order) => {
-        const orderTime = new Date(order.orderTime);
-        const today = new Date();
+  // Function to determine the status based on pickUpTime and returnTime
+  const getStatus = (order) => {
+    const orderTime = new Date(order.orderTime);
+    const today = new Date();
+    const timeDifference = today.getTime() - orderTime.getTime();
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
 
-        // Calculate the difference in milliseconds between the current date and orderTime
-        const timeDifference = today.getTime() - orderTime.getTime();
+    if (!order.pickUpTime && !order.returnTime) {
+      return daysDifference > 1 ? 'Order Canceled' : 'Not Picked Up';
+    } else if (order.pickUpTime && !order.returnTime) {
+      return 'Picked Up';
+    } else if (order.pickUpTime && order.returnTime) {
+      return 'Returned';
+    } else {
+      return 'Unknown';
+    }
+  };
 
-        // Calculate the number of days between today and orderTime
-        const daysDifference = timeDifference / (1000 * 3600 * 24);
-
-        if (!order.pickUpTime && !order.returnTime) {
-            return daysDifference > 1 ? "Order Canceled" : "Not Picked Up";
-        } else if (order.pickUpTime && !order.returnTime) {
-            return "Picked Up";
-        } else if (order.pickUpTime && order.returnTime) {
-            return "Returned";
-        } else {
-            return "Unknown";
-        }
+  const formatDate = (dateString) => {
+    const options = {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
     };
+    return new Date(dateString).toLocaleString(undefined, options);
+  };
 
-    const formatDate = (dateString) => {
-        const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-        return new Date(dateString).toLocaleString(undefined, options);
-    };
-
-    return (
-        <div className="w-screen bg-gray-100">
-            <Navbar />
-            <div className="container mx-auto px-4 py-8">
-                <h1 className="text-3xl font-semibold mb-6">Order History</h1>
-                <div className="hidden md:block">
-                    {/* Table for desktop screens (md and above) */}
-                    {/* ... (rest of the desktop table code) ... */}
-                </div>
-                <div className="md:hidden">
-                    {/* Cards for mobile screens (sm and below) */}
-                    <div className="grid grid-cols-1 gap-4">
-                        {orders.map((order) => (
-                            <div key={order._id} className="bg-white shadow-md rounded-lg overflow-hidden">
-                                <div className="p-4">
-                                    <div className="font-semibold mb-2">Car</div>
-                                    <div>{`${order.carId.make} ${order.carId.model}`}</div>
-                                </div>
-                                <div className="p-4">
-                                    <div className="font-semibold mb-2">Order Time</div>
-                                    <div>{formatDate(order.orderTime)}</div>
-                                </div>
-                                <div className="p-4">
-                                    <div className="font-semibold mb-2">Pickup Time</div>
-                                    <div>
-                                        {order.pickUpTime ? (
-                                            <span>
-                                                {formatDate(order.pickUpTime)} {getStatus(order) === 'Returned' && <span className="text-green-600 ml-1">✓</span>}
-                                            </span>
-                                        ) : (
-                                            "Not Picked Up"
-                                        )}
-                                    </div>
-                                </div>
-                                <div className="p-4">
-                                    <div className="font-semibold mb-2">Return Time</div>
-                                    <div>
-                                        {order.returnTime ? (
-                                            <span>
-                                                {formatDate(order.returnTime)} {getStatus(order) === 'Returned' && <span className="text-green-600 ml-1">✓</span>}
-                                            </span>
-                                        ) : (
-                                            "Not Returned"
-                                        )}
-                                    </div>
-                                </div>
-                                <div className={`px-4 py-3 ${getStatus(order) === 'Returned' ? 'bg-green-600 text-white' : 'bg-yellow-600 text-gray-800'}`}>
-                                    {getStatus(order)}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="w-screen bg-gray-100">
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-semibold mb-6">Order History</h1>
+        {/* Table for desktop screens (md and above) */}
+        <div className="md:block hidden">
+          <table className="table-auto w-full">
+            {/* Table headers */}
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Car</th>
+                <th className="px-4 py-2">License Plate</th>
+                <th className="px-4 py-2">Order Time</th>
+                <th className="px-4 py-2">Pickup Time</th>
+                <th className="px-4 py-2">Return Time</th>
+                <th className="px-4 py-2">Status</th>
+              </tr>
+            </thead>
+            {/* Table body */}
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td className="border px-4 py-2">
+                    {`${order.carId.make} ${order.carId.model}`}
+                  </td>
+                  <td className="border px-4 py-2">{order.carId.licensePlate}</td>
+                  <td className="border px-4 py-2">
+                    {formatDate(order.orderTime)}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {order.pickUpTime ? (
+                      <span>
+                        {formatDate(order.pickUpTime)}{' '}
+                        {getStatus(order) === 'Returned' && (
+                          <span className="text-green-600 ml-1">✓</span>
+                        )}
+                      </span>
+                    ) : (
+                      'Not Picked Up'
+                    )}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {order.returnTime ? (
+                      <span>
+                        {formatDate(order.returnTime)}{' '}
+                        {getStatus(order) === 'Returned' && (
+                          <span className="text-green-600 ml-1">✓</span>
+                        )}
+                      </span>
+                    ) : (
+                      'Not Returned'
+                    )}
+                  </td>
+                  <td
+                    className={`border px-4 py-2 ${
+                      getStatus(order) === 'Returned'
+                        ? 'bg-green-600 text-white'
+                        : 'bg-yellow-600 text-gray-800'
+                    }`}
+                  >
+                    {getStatus(order)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+        {/* Cards for mobile screens (sm and below) */}
+        <div className="md:hidden block">
+          <div className="grid grid-cols-1 gap-4">
+            {orders.map((order) => (
+              <div
+                key={order._id}
+                className="bg-white shadow-md rounded-lg overflow-hidden"
+              >
+                {/* Card content */}
+                <div className="p-4">
+                  <div className="font-semibold mb-2">Car</div>
+                  <div>{`${order.carId.make} ${order.carId.model}`}</div>
+                </div>
+                <div className="p-4">
+                  <div className="font-semibold mb-2">License Plate</div>
+                  <div>{order.carId.licensePlate}</div>
+                </div>
+                <div className="p-4">
+                  <div className="font-semibold mb-2">Order Time</div>
+                  <div>{formatDate(order.orderTime)}</div>
+                </div>
+                <div className="p-4">
+                  <div className="font-semibold mb-2">Pickup Time</div>
+                  <div>
+                    {order.pickUpTime ? (
+                      <span>
+                        {formatDate(order.pickUpTime)}{' '}
+                        {getStatus(order) === 'Returned' && (
+                          <span className="text-green-600 ml-1">✓</span>
+                        )}
+                      </span>
+                    ) : (
+                      'Not Picked Up'
+                    )}
+                  </div>
+                </div>
+                <div className="p-4">
+                  <div className="font-semibold mb-2">Return Time</div>
+                  <div>
+                    {order.returnTime ? (
+                      <span>
+                        {formatDate(order.returnTime)}{' '}
+                        {getStatus(order) === 'Returned' && (
+                          <span className="text-green-600 ml-1">✓</span>
+                        )}
+                      </span>
+                    ) : (
+                      'Not Returned'
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={`px-4 py-3 ${
+                    getStatus(order) === 'Returned'
+                      ? 'bg-green-600 text-white'
+                      : 'bg-yellow-600 text-gray-800'
+                  }`}
+                >
+                  {getStatus(order)}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
