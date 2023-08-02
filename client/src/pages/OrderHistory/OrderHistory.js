f76b66da714707e2a9ca8145a504b42632482703
nmemarcoding <nmemarcoding@gmail.com>
2023-08-02 09:37:36 -0700

    Updated OrderHistory component:   - Fetch data from the API   - Removed the ID column from the table and cards   - Modified order status display based on pickUpTime and returnTime   - Display newer orders at the top of the table and cards

--- client/src/pages/OrderHistory/OrderHistory.js
+++ client/src/pages/OrderHistory/OrderHistory.js
@@ -1,73 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import useStore from '../../store';
+import { publicRequest } from '../../hooks/requestMethods';
 
 export default function OrderHistory() {
-  const orders = [
-    { id: 1, car: 'Toyota Camry', pickupTime: '2022-01-01 10:00 AM', returnTime: '2022-01-02 10:00 AM', status: 'Completed' },
-    { id: 2, car: 'Honda Civic', pickupTime: '2022-01-03 10:00 AM', returnTime: '2022-01-04 10:00 AM', status: 'In Progress' },
-    { id: 3, car: 'Nissan Altima', pickupTime: '2022-01-05 10:00 AM', returnTime: '2022-01-06 10:00 AM', status: 'Cancelled' },
-  ];
+    const userInfo = useStore((state) => state?.userInfo);
+    const [userId, setUserId] = useState(useStore((state) => state?.userInfo?._id))
+    const [orders, setOrders] = useState([]);
 
-  return (
-    <div className="w-screen bg-gray-100">
-      <Navbar />
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-semibold mb-6">Order History</h1>
-        <div className="hidden md:block">
-          {/* Table for desktop screens (md and above) */}
-          <table className="w-full bg-white shadow-md rounded-lg overflow-hidden">
-            <thead>
-              <tr className="text-left text-gray-600">
-                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">ID</th>
-                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Car</th>
-                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Pickup Time</th>
-                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Return Time</th>
-                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order.id} className="border-t border-gray-200">
-                  <td className="py-4 px-4">{order.id}</td>
-                  <td className="py-4 px-4">{order.car}</td>
-                  <td className="py-4 px-4">{order.pickupTime}</td>
-                  <td className="py-4 px-4">{order.returnTime}</td>
-                  <td className={`py-4 px-4 ${order.status === 'Completed' ? 'text-green-600' : 'text-yellow-600'}`}>
-                    {order.status}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    useEffect(() => { 
+        publicRequest().get(`/order/getallorders?userid=${userId}`)
+        .then((res) => {
+            // Assuming the API response is an array of orders sorted by orderTime (newest first)
+            const sortedOrders = res.data.sort((a, b) => new Date(b.orderTime) - new Date(a.orderTime));
+            setOrders(sortedOrders);
+        })
+        .catch((err) => console.log(err))
+    }, [userId]); // Add the userId dependency to fetch orders when the userId changes
+
+    // Function to determine the status based on pickUpTime and returnTime
+    const getStatus = (order) => {
+        if (!order.pickUpTime && !order.returnTime) {
+            return "Not Picked Up";
+        } else if (order.pickUpTime && !order.returnTime) {
+            return "Picked Up";
+        } else if (order.pickUpTime && order.returnTime) {
+            return "Returned";
+        } else {
+            return "Unknown";
+        }
+    };
+
+    return (
+        <div className="w-screen bg-gray-100">
+            <Navbar />
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-3xl font-semibold mb-6">Order History</h1>
+                <div className="hidden md:block">
+                    {/* Table for desktop screens (md and above) */}
+                    <table className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+                        <thead>
+                            <tr className="text-left text-gray-600">
+                                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Car</th>
+                                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Pickup Time</th>
+                                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Return Time</th>
+                                <th className="py-3 px-4 bg-gray-200 font-semibold uppercase">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orders.map((order) => (
+                                <tr key={order._id} className="border-t border-gray-200">
+                                    <td className="py-4 px-4">{`${order.carId.make} ${order.carId.model}`}</td>
+                                    <td className="py-4 px-4">{order.pickUpTime || "Not Picked Up"}</td>
+                                    <td className="py-4 px-4">{order.returnTime || "Not Returned"}</td>
+                                    <td className={`py-4 px-4 ${getStatus(order) === 'Returned' ? 'text-green-600' : 'text-yellow-600'}`}>
+                                        {getStatus(order)}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="md:hidden">
+                    {/* Cards for mobile screens (sm and below) */}
+                    <div className="grid grid-cols-1 gap-4">
+                        {orders.map((order) => (
+                            <div key={order._id} className="bg-white shadow-md rounded-lg overflow-hidden">
+                                <div className="p-4">
+                                    <div className="font-semibold mb-2">Car</div>
+                                    <div>{`${order.carId.make} ${order.carId.model}`}</div>
+                                </div>
+                                <div className="p-4">
+                                    <div className="font-semibold mb-2">Pickup Time</div>
+                                    <div>{order.pickUpTime || "Not Picked Up"}</div>
+                                </div>
+                                <div className="p-4">
+                                    <div className="font-semibold mb-2">Return Time</div>
+                                    <div>{order.returnTime || "Not Returned"}</div>
+                                </div>
+                                <div className={`px-4 py-3 ${getStatus(order) === 'Returned' ? 'bg-green-600 text-white' : 'bg-yellow-600 text-gray-800'}`}>
+                                    {getStatus(order)}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="md:hidden">
-          {/* Cards for mobile screens (sm and below) */}
-          <div className="grid grid-cols-1 gap-4">
-            {orders.map((order) => (
-              <div key={order.id} className="bg-white shadow-md rounded-lg overflow-hidden">
-                <div className="px-4 py-3 bg-gray-200 font-semibold uppercase">
-                  ID: {order.id}
-                </div>
-                <div className="p-4">
-                  <div className="font-semibold mb-2">Car</div>
-                  <div>{order.car}</div>
-                </div>
-                <div className="p-4">
-                  <div className="font-semibold mb-2">Pickup Time</div>
-                  <div>{order.pickupTime}</div>
-                </div>
-                <div className="p-4">
-                  <div className="font-semibold mb-2">Return Time</div>
-                  <div>{order.returnTime}</div>
-                </div>
-                <div className={`px-4 py-3 ${order.status === 'Completed' ? 'bg-green-600 text-white' : 'bg-yellow-600 text-gray-800'}`}>
-                  {order.status}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
