2bf775c3ce3f468d0f4f38fbb69e5e009612a3f6
nmemarcoding <nmemarcoding@gmail.com>
2023-08-15 10:21:19 -0700

    Enhance AdminReservedCarTable functionality: Prioritize Returned status over Cancelled and display car availability status as Available or Not Available.

--- client/src/components/AdminReservedCarTable/AdminReservedCarTable.js
+++ client/src/components/AdminReservedCarTable/AdminReservedCarTable.js
@@ -6,7 +6,7 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
   };
   
   const returnOnClick = () => {
-    handelReturn(order._id)
+    handelReturn(order._id);
   };
   
   // Function to check if orderTime is more than one day ago
@@ -16,7 +16,38 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
     const currentTimeInMillis = new Date().getTime();
     return currentTimeInMillis - orderTimeInMillis > oneDayInMillis;
   };
-  
+
+  const displayActions = () => {
+    // Check for "Returned" status first
+    if (order.returnTime && order.pickUpTime) {
+        return <div className=''>Returned</div>;
+    }
+    // If not "Returned", then check for "Cancelled"
+    if (isOrderCancelled()) {
+        return <div className='text-red-600'>Cancelled</div>;
+    }
+    if (order.pickUpTime && !order.returnTime && !order.carId.available) {
+      return (
+        <button
+          className='px-2 py-1 rounded bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:shadow-outline-red'
+          onClick={returnOnClick}
+        >
+          Return
+        </button>
+      );
+    }
+    if (order.carId.available && !order.returnTime && !order.pickUpTime) {
+      return (
+        <button
+          className='px-2 py-1 rounded bg-yellow-500 text-white hover:bg-yellow-700 focus:outline-none focus:shadow-outline-yellow'
+          onClick={pickUpOnClick}
+        >
+          Pick Up
+        </button>
+      );
+    }
+  };
+
   return (
     <div className='flex flex-col items-center sm:items-start w-full px-4 sm:px-0'>
       <div className='w-full overflow-auto sm:hidden'>
@@ -38,29 +69,9 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
           </div>
           <div className='mb-2'>
             <strong className='font-bold'>Status: </strong>
-            {isOrderCancelled() ? 'Cancelled' : (order.carId.available ? 'Available' : 'Not Available')}
+            {order.carId.available ? 'Available' : 'Not Available'}
           </div>
-          {order.carId.available && !order.returnTime && !order.pickUpTime && !isOrderCancelled() && (
-            <button
-              className='px-2 py-1 rounded bg-yellow-500 text-white hover:bg-yellow-700 focus:outline-none focus:shadow-outline-yellow'
-              onClick={pickUpOnClick}
-            >
-              Pick Up
-            </button>
-          )}
-          {order.returnTime && order.pickUpTime && (
-            <div className=''>
-              Returned
-            </div>
-          )}
-          {order.pickUpTime && !order.returnTime && !order.carId.available && !isOrderCancelled() && (
-            <button
-              className='px-2 py-1 rounded bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:shadow-outline-red'
-              onClick={returnOnClick}
-            >
-              Return
-            </button>
-          )}
+          {displayActions()}
         </div>
       </div>
       <div className='hidden sm:block w-full'>
@@ -81,34 +92,9 @@ export default function AdminReservedCarTable({ order, onPickUp, handelPickUp, h
               <td className='px-2 sm:px-4 py-2'>{`${order.userId?.firstName} ${order.userId?.lastName}`}</td>
               <td className='px-2 sm:px-4 py-2'>{`${order.carId.make} ${order.carId.model}`}</td>
               <td className='px-2 sm:px-4 py-2'>{new Date(order.orderTime).toLocaleDateString('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'})} Until 6 PM</td>
-              <td className='px-2 sm:px-4 py-2'>{isOrderCancelled() ? 'Cancelled' : (order.carId.available ? 'Available' : 'Not Available')}</td>
+              <td className='px-2 sm:px-4 py-2'>{order.carId.available ? 'Available' : 'Not Available'}</td>
               <td className='px-2 sm:px-4 py-2'>
-                {order.carId.available && !order.returnTime && !order.pickUpTime && !isOrderCancelled() && (
-                  <button
-                    className='px-2 py-1 rounded bg-yellow-500 text-white hover:bg-yellow-700 focus:outline-none focus:shadow-outline-yellow'
-                    onClick={pickUpOnClick}
-                  >
-                    Pick Up
-                  </button>
-                )}
-                {order.returnTime && order.pickUpTime && (
-                  <div className=''>
-                    Returned
-                  </div>
-                )}
-                {order.pickUpTime && !order.returnTime && !order.carId.available && !isOrderCancelled() && (
-                  <button
-                    className='px-2 py-1 rounded bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:shadow-outline-red'
-                    onClick={returnOnClick}
-                  >
-                    Return
-                  </button>
-                )}
-                {isOrderCancelled() && (
-                  <div className='text-red-600'>
-                    Cancelled
-                  </div>
-                )}
+                {displayActions()}
               </td>
             </tr>
           </tbody>
